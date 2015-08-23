fd2bcf63e3506f25ba09dca9759bf92584866dfb
cybergames1 <jtliu.tju@gmail.com>
2015-08-23 09:43:03 +0800

    calendar

--- LimaHeBox/Classes/Controllers/Found/Calendar/CalendarViewController.m
+++ LimaHeBox/Classes/Controllers/Found/Calendar/CalendarViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "CalendarViewController.h"
-#import "VRGCalendarView.h"
+#import "CalendarView.h"
 
-@interface CalendarViewController () <VRGCalendarViewDelegate>
+@interface CalendarViewController () <CalendarDelegate>
 
 @end
 
@@ -20,20 +20,32 @@
     [self setNavigationTitle:@"日历"];
     self.view.backgroundColor = self.navigationBarTintColor;
     
-    VRGCalendarView *calendar = [[VRGCalendarView alloc] init];
-    calendar.delegate = self;
+    CalendarView *calendar = [[CalendarView alloc]initWithFrame:CGRectMake(0, 64, self.view.width, self.view.width)];
+    calendar.delegate    = self;
+    calendar.calendarDate                = [NSDate date];
+    calendar.monthAndDayTextColor        = [UIColor whiteColor];
+    calendar.dayBgColorWithData          = [UIColor clearColor];
+    calendar.dayBgColorWithoutData       = [UIColor clearColor];
+    calendar.dayBgColorSelected          = [UIColor clearColor];
+    calendar.dayTxtColorWithoutData      = [UIColor whiteColor];
+    calendar.dayTxtColorWithData         = [UIColor whiteColor];
+    calendar.dayTxtColorSelected         = [UIColor whiteColor];
+    calendar.borderColor                 = [UIColor clearColor];
+    calendar.borderWidth                 = 1;
+    calendar.allowsChangeMonthByDayTap   = YES;
+    calendar.allowsChangeMonthByButtons  = YES;
+    calendar.keepSelDayWhenMonthChange   = YES;
+//    calendar.nextMonthAnimation          = UIViewAnimationOptionTransitionFlipFromRight;
+//    calendar.prevMonthAnimation          = UIViewAnimationOptionTransitionFlipFromLeft;
+    calendar.center = CGPointMake(self.view.center.x, calendar.center.y);
     [self.view addSubview:calendar];
 }
 
--(void)calendarView:(VRGCalendarView *)calendarView switchedToMonth:(int)month targetHeight:(float)targetHeight animated:(BOOL)animated {
-    if (month==[[NSDate date] month]) {
-        NSArray *dates = [NSArray arrayWithObjects:[NSNumber numberWithInt:1],[NSNumber numberWithInt:5], nil];
-        [calendarView markDates:dates];
-    }
-}
+#pragma mark - CalendarDelegate protocol conformance
 
--(void)calendarView:(VRGCalendarView *)calendarView dateSelected:(NSDate *)date {
-    NSLog(@"Selected date = %@",date);
+-(void)dayChangedToDate:(NSDate *)selectedDate
+{
+    NSLog(@"dayChangedToDate %@(GMT)",selectedDate);
 }
 
 @end
