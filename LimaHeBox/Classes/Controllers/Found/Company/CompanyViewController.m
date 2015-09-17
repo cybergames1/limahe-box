58ab7c2b7d81d748537f3ac27c3562256e2a0228
cybergames1 <jtliu.tju@gmail.com>
2015-09-17 11:14:28 +0800

    修改公司简介

--- LimaHeBox/Classes/Controllers/Found/Company/CompanyViewController.m
+++ LimaHeBox/Classes/Controllers/Found/Company/CompanyViewController.m
@@ -7,15 +7,6 @@
 //
 
 #import "CompanyViewController.h"
-#import "NewsDataSource.h"
-#import "CompanyNewsListViewController.h"
-
-@interface CompanyViewController ()
-{
-    UITextView * _textView;
-}
-
-@end
 
 @implementation CompanyViewController
 
@@ -23,52 +14,13 @@
     [super viewDidLoad];
     [self setNavigationTitle:@"公司介绍"];
     
-    UITextView *textView = [[[UITextView alloc] initWithFrame:CGRectMake(0, 100, self.view.width, self.view.height-54-100)] autorelease];
-    textView.backgroundColor = [UIColor clearColor];
-    [self.view addSubview:textView];
-    _textView = textView;
+    UIWebView *webView = [[[UIWebView alloc] initWithFrame:self.view.bounds] autorelease];
+    [self.view addSubview:webView];
     
-    UIButton *button = [[[UIButton alloc] initWithFrame:CGRectMake(0, self.view.height-44, self.view.width/2, 44)] autorelease];
-    [button setTitle:@"产品展示" forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-    [button setBackgroundColor:UIColorRGB(248, 248, 248)];
-    [button addTarget:self action:@selector(showProductListAction) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.liemoch.com/mobile/article_cat.php?id=11"] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0]];
     
-    button = [[[UIButton alloc] initWithFrame:CGRectMake(self.view.width/2, self.view.height-44, self.view.width/2, 44)] autorelease];
-    [button setTitle:@"公司动态" forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-    [button setBackgroundColor:UIColorRGB(248, 248, 248)];
-    [button addTarget:self action:@selector(showCompanyNewsAction) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
-    
-    UIView *line = [[[UIView alloc] initWithFrame:CGRectMake(self.view.width/2, button.top, 1, button.height)] autorelease];
-    line.backgroundColor = UIColorRGB(229, 229, 229);
-    [self.view addSubview:line];
-    
-    NewsDataSource *dataSource_ = [[NewsDataSource alloc] initWithDelegate:self];
-    [dataSource_ getNewsInfoWithId:[NSString stringWithFormat:@"%d",20]];
-    self.dataSource = dataSource_;
+    return;
 }
 
-- (void)showProductListAction {
-    
-}
-
-- (void)showCompanyNewsAction {
-    CompanyNewsListViewController *controller = [[CompanyNewsListViewController alloc] init];
-    [self.navigationController pushViewController:controller animated:YES];
-    [controller release];
-}
-
-- (void)dataSourceFinishLoad:(PPQDataSource *)source {
-    NSString *content = [[source.data objectForKey:@"data"] objectForKey:@"content"];
-    _textView.text = content;
-    
-}
-
-- (void)dataSource:(PPQDataSource *)source hasError:(NSError *)error {
-    
-}
 
 @end
